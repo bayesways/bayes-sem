171c8286f06589ea7da21fb192db831dfe615bd3
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-01-25 18:13:09 -0500

    working code

--- src/codebase/gibbs.py
+++ src/codebase/gibbs.py
@@ -10,24 +10,25 @@ def trunc_normal(i, mean, cov, max_counter=10):
         counter = 0
         while a<=0.:
             if counter >= max_counter:
-                print("No sample")
-                return 0.
+                # print("No sample")
+                return np.array(0.)
             else:
                 a = multivariate_normal.rvs(mean, cov)
                 counter+=1
-        return a
+        return np.array(a)
 
     else:
         a = np.zeros(mean.shape[0])
         counter = 0
         while a[i]<=0.:
             if counter >= max_counter:
-                print("No sample")
+                # print("No sample")
                 return np.zeros(mean.shape[0])
             else:
                 a = multivariate_normal.rvs(mean, cov)
                 counter += 1
-        return a
+        return np.array(a)
+
 
 def create_w_columns(i,k,ww):
     if i+1<=k:
@@ -44,13 +45,14 @@ def mcmc(data, nsim=100):
     sigma_s = np.empty((nsim, data['J']))
     z_s = np.empty((nsim, data['N'], data['K']))
 
-    # sigma_temp = np.ones(data['J'])
-    sigma_temp = data['sigma'].copy()
+    sigma_temp = np.ones(data['J'])
+    # sigma_temp = data['sigma'].copy()
     Sigma_temp = np.diag(sigma_temp)
 
     zz_temp = data['z'].copy()
-    ww_temp = data['w'].copy()
-    # ww_temp = norm.rvs(size=data['J']*data['K']).reshape((data['J'],data['K']))
+    # ww_temp = data['w'].copy()
+    ww_temp = norm.rvs(size=data['J']*data['K']).reshape((data['J'],data['K']))
+    ww_temp[0,1] = 0.
 
     C0 = 1e2
     mu0 = 1
@@ -60,9 +62,9 @@ def mcmc(data, nsim=100):
     for j in range(nsim):
         # sample z
         zz_temp = np.empty((data['N'], data['K']))
+        inv_Sigma = inv(Sigma_temp)
+        cov1 = inv(np.eye(data['K']) +  ww_temp.T @ inv_Sigma @ ww_temp)
         for t in range(data['N']):
-            inv_Sigma = inv(Sigma_temp)
-            cov1 = inv(np.eye(data['K']) +  ww_temp.T @ inv_Sigma @ ww_temp)
             mean = cov1 @ ww_temp.T @ inv_Sigma @ data['y'][t]
             zz_temp[t] = multivariate_normal.rvs(mean, cov1)
         z_s[j] = zz_temp
@@ -75,6 +77,8 @@ def mcmc(data, nsim=100):
             sigma_b = (prior_a*(prior_b**2)+d)/2.
             sigma_temp[i] = invgamma.rvs(sigma_a, scale = sigma_b)
         sigma_s[j] = sigma_temp
+        Sigma_temp = np.diag(sigma_temp)
+
 
         # sample w
         for i in range(data['J']):
@@ -84,8 +88,8 @@ def mcmc(data, nsim=100):
                 C = inv(inv_C)
                 aux2= zz_temp[:,:(i+1)].T @ data['y'][:,i]
                 mean = C @ (C0**(-1)*mu0*np.ones(i+1) + sigma_temp[i]**(-2)*aux2 )
-                # if mean[i] > 0 or True:
-                ww_temp[i,:(i+1)] = trunc_normal(i, mean, C)
+                if mean[i] > 0:
+                    ww_temp[i,:(i+1)] = trunc_normal(i, mean, C)
 
             else:
                 aux1= zz_temp.T @ zz_temp
