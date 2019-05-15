4879705589380876da2f7f6172a1c0f7020d54d2
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-05-14 23:37:33 -0400

    added muthen experiment1

--- src/run_muthen_exp1.py
+++ src/run_muthen_exp1.py
@@ -1,29 +1,29 @@
 import numpy as np
 import pandas as pd
 import pystan
-from scipy.stats import norm, multivariate_normal, invwishart, invgamma
-from statsmodels.tsa.stattools import acf
 import datetime
 import sys
 import os
 
 from codebase.file_utils import save_obj, load_obj
-from codebase.post_process import *
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("num_warmup", help="number of warm up iterations", type=int, default=1000)
 parser.add_argument("num_samples", help="number of post-warm up iterations", type=int, default=1000)
 parser.add_argument("num_chains", help="number of MCMC chains", type=int, default=1)
+parser.add_argument("gender", help="run men or women", type=str, default="men")
 # Optional arguments
 parser.add_argument("-th", "--task_handle", help="hande for task", type=str, default="_")
+parser.add_argument("-prm", "--print_model", help="print model on screen", type=bool, default=False)
 parser.add_argument("-xdir", "--existing_directory", help="refit compiled model in existing directory",
     type=str, default=None)
 
 args = parser.parse_args()
 
+print("Reading data for %s"%args.gender)
+df = pd.read_csv("../dat/muthen_"+args.gender+".csv")
 
-df = pd.read_csv("../dat/muthen_women.csv")
 
 
 data = dict()
@@ -31,30 +31,39 @@ data['N'] = df.shape[0]
 data['K'] = 5
 data['J'] = df.shape[1]
 data['y'] = df.values
-
+print("N = %d, J= %d, K =%d"%(data['N'],data['J'], data['K'] ))
 
 stan_data = dict(N = data['N'], K = data['K'], J = data['J'], yy = data['y'])
 
 with open('./codebase/stan_code/cont/CFA/marg_m.stan', 'r') as file:
     model_code = file.read()
-print(model_code)
+if args.print_model == True:
+    print(model_code)
 
 
 if args.existing_directory is None:
     nowstr = datetime.datetime.now().strftime('%Y%m%d_%H%M%S_') # ISO 8601 format
     log_dir =  "./log/"+nowstr+"%s/" % args.task_handle
+    print("Compiling model")
     sm = pystan.StanModel(model_code=model_code, verbose=False)
 
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
+    print("Saving compiled model in directory %s"%log_dir)
     save_obj(sm, 'sm', log_dir)
+
 
 else:
     log_dir = "./log/"+args.existing_directory
+    print("Reading existing compiled model from directory %s"%log_dir)
     sm = load_obj('sm', log_dir)
+
+
+print("Fitting model.... \n\n")
 
 fit_run = sm.sampling(data=stan_data,
     iter=args.num_samples + args.num_warmup,
     warmup=args.num_warmup, chains=args.num_chains)
 
+print("Saving fitted model in directory %s"%log_dir)
 save_obj(fit_run, 'fit', log_dir)
