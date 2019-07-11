fbffc27bc1dd46d81346fb6121af27ec3e5e94ca
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-07-11 18:08:38 -0400

    updated code to run experiments on fabian

--- src/run_muthen_exp1.py
+++ src/run_muthen_exp1.py
@@ -13,8 +13,8 @@ parser.add_argument("num_warmup", help="number of warm up iterations", type=int,
 parser.add_argument("num_samples", help="number of post-warm up iterations", type=int, default=1000)
 parser.add_argument("num_chains", help="number of MCMC chains", type=int, default=1)
 parser.add_argument("gender", help="run men or women", type=str, default="men")
+parser.add_argument("stan_model", help="0:full model, 1:no u's, 2: no u's no approx zero betas ", type=int, default=0)
 # Optional arguments
-parser.add_argument("-uu", "--use_u", help="print model on screen", type=int, default=1)
 parser.add_argument("-th", "--task_handle", help="hande for task", type=str, default="_")
 parser.add_argument("-prm", "--print_model", help="print model on screen", type=int, default=0)
 parser.add_argument("-xdir", "--existing_directory", help="refit compiled model in existing directory",
@@ -22,53 +22,81 @@ parser.add_argument("-xdir", "--existing_directory", help="refit compiled model 
 
 args = parser.parse_args()
 
-print("\n\nReading data for %s"%args.gender)
-df = pd.read_csv("../dat/muthen_"+args.gender+".csv")
-df = df.replace(-9, np.nan).astype(float)
-df.dropna(inplace=True)
-df = df.astype(int)
-
-
-data = dict()
-data['N'] = df.shape[0]
-data['K'] = 5
-data['J'] = df.shape[1]
-data['y'] = df.values
-print("\n\nN = %d, J= %d, K =%d"%(data['N'],data['J'], data['K'] ))
-
-stan_data = dict(N = data['N'], K = data['K'], J = data['J'], yy = data['y'])
-
-if bool(args.use_u) :
-    with open('./codebase/stan_code/cont/CFA/marg_m.stan', 'r') as file:
-        model_code = file.read()
-else:
-    with open('./codebase/stan_code/cont/CFA/marg_m_nou.stan', 'r') as file:
-        model_code = file.read()
-if bool(args.print_model):
-    print(model_code)
-
-
+############################################################
+###### Create Directory or Open existing ##########
 if args.existing_directory is None:
     nowstr = datetime.datetime.now().strftime('%Y%m%d_%H%M%S_') # ISO 8601 format
     log_dir =  "./log/"+nowstr+"%s/" % args.task_handle
-    print("\n\nCompiling model")
-    sm = pystan.StanModel(model_code=model_code, verbose=False)
-
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    print("\n\nSaving compiled model in directory %s"%log_dir)
-    save_obj(sm, 'sm', log_dir)
-
-
 else:
     log_dir = args.existing_directory
     if log_dir[-1] != "/":
         print("\n\nAppending `/`-character at the end of directory")
         log_dir = log_dir+ "/"
+
+############################################################
+################ Create Data or Load ##########
+if args.existing_directory is None:
+    print("\n\nReading data for %s"%args.gender)
+    df = pd.read_csv("../dat/muthen_"+args.gender+".csv")
+    df = df.replace(-9, np.nan).astype(float)
+    df.dropna(inplace=True)
+    df = df.astype(int)
+    data = dict()
+    data['N'] = df.shape[0]
+    data['K'] = 5
+    data['J'] = df.shape[1]
+    data['y'] = df.values
+    print("\n\nN = %d, J= %d, K =%d"%(data['N'],data['J'], data['K'] ))
+
+    stan_data = dict(N = data['N'], K = data['K'], J = data['J'], yy = data['y'])
+    print("\n\nSaving data to directory %s"% log_dir)
+    save_obj(stan_data, 'stan_data', log_dir)
+
+else:
+    print("\n\nReading data from directory %s"%  log_dir)
+    stan_data = load_obj("stan_data", log_dir)
+
+
+############################################################
+################ Compile Model or Load ##########
+if args.existing_directory is None:
+
+    print("\n\nReading Stan Code from model %d" % args.stan_model)
+    if args.stan_model == 0 :
+        with open('./codebase/stan_code/cont/CFA/marg_m.stan', 'r') as file:
+            model_code = file.read()
+    elif args.stan_model == 1 :
+        with open('./codebase/stan_code/cont/CFA/marg_m_nou.stan', 'r') as file:
+            model_code = file.read()
+    elif args.stan_model == 2 :
+        with open('./codebase/stan_code/cont/CFA/marg_m_nou_no_approxbeta.stan', 'r') as file:
+            model_code = file.read()
+    else:
+        print("Choose stan model {0:full model, 1:no u's, 2: no u's no approx zero betas}")
+
+    if bool(args.print_model):
+        print(model_code)
+    file = open(log_dir+"model.txt", "w") 
+    file.write(model_code)
+    file.close()
+
+
+    print("\n\nCompiling model")
+    sm = pystan.StanModel(model_code=model_code, verbose=False)
+
+    print("\n\nSaving compiled model in directory %s"%log_dir)
+    save_obj(sm, 'sm', log_dir)
+
+
+else:
     print("\n\nReading existing compiled model from directory %s"%log_dir)
     sm = load_obj('sm', log_dir)
 
 
+############################################################
+################ Fit Model ##########
 print("\n\nFitting model.... \n\n")
 
 fit_run = sm.sampling(data=stan_data,
@@ -80,9 +108,10 @@ save_obj(fit_run, 'fit', log_dir)
 
 
 print("\n\nSaving posterior samples in %s"%log_dir)
-param_names = ['Omega_beta', 'beta', 'V_corr', 'V' , 'alpha', 'sigma', 'sigma_z']
-if bool(args.use_u) :
-    param_names.append('uu')
+param_names = ['Marg_cov',  'beta', 'Phi_cov', 'sigma', 'sigma_z', 'alpha', "Theta"]
+
+if args.stan_model == 0 :
+    param_names.extend(['uu', 'Omega', 'Marg_cov2'])
 
 stan_samples= fit_run.extract(permuted=False, pars=param_names)  # return a dictionary of arrays
 
