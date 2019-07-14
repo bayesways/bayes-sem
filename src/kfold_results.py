9b0cf22fe337a839e3811b63ad4e0de1443a76f7
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-07-14 17:28:01 -0400

    minor

--- src/kfold_results.py
+++ src/kfold_results.py
@@ -11,7 +11,9 @@ from codebase.file_utils import save_obj, load_obj
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("logdir", help="path to files", type=str, default=None)
+parser.add_argument("logdir_1", help="path to files", type=str, default=None)
+parser.add_argument("logdir_2", help="path to files", type=str, default=None)
+
 # Optional arguments
 parser.add_argument("-prm", "--print_model", help="print model on screen", type=int, default=1)
 
@@ -20,26 +22,58 @@ args = parser.parse_args()
 print("\n\nPrinting Stan model code \n\n")
 
 
-log_dir = args.logdir
-if log_dir[-1] != "/":
+log_dir1 = args.logdir_1
+if log_dir1[-1] != "/":
     print("\n\nAppending `/`-character at the end of directory")
-    log_dir = log_dir+ "/"
+    log_dir1 = log_dir1+ "/"
 
 if bool(args.print_model):
-    file = open(log_dir+"model.txt", "r")
+    file = open(log_dir1+"model.txt", "r")
     print(file.read())
     file.close()
+
+log_dir2 = args.logdir_2
+if log_dir2[-1] != "/":
+    print("\n\nAppending `/`-character at the end of directory")
+    log_dir2 = log_dir2+ "/"
+
+if bool(args.print_model):
+    file = open(log_dir2+"model.txt", "r")
+    print(file.read())
+    file.close()
+
 
 ############################################################
 ################ Load Data and models  ##########
 
 print("\n\nLoading files...\n\n")
-complete_data = load_obj("complete_data", log_dir)
+
+
+print("\n\nChecking data integrity...\n\n")
+complete_data = load_obj("complete_data", log_dir1)
+complete_data2 = load_obj("complete_data", log_dir2)
+
+for fold_index in range(3):
+    np.testing.assert_equal(complete_data[fold_index]['test']['yy'],\
+        complete_data2[fold_index]['test']['yy'] )
+    np.testing.assert_equal(complete_data[fold_index]['train']['yy'],\
+        complete_data2[fold_index]['train']['yy'] )
+
+
+
+model_posterior_samples = dict()
 
 ps = dict()
-ps[0] = load_obj('ps_0', log_dir)
-ps[1] = load_obj('ps_1', log_dir)
-ps[2] = load_obj('ps_2', log_dir)
+ps[0] = load_obj('ps_0', log_dir1)
+ps[1] = load_obj('ps_1', log_dir1)
+ps[2] = load_obj('ps_2', log_dir1)
+model_posterior_samples[1] = ps
+
+ps = dict()
+ps[0] = load_obj('ps_0', log_dir2)
+ps[1] = load_obj('ps_1', log_dir2)
+ps[2] = load_obj('ps_2', log_dir2)
+model_posterior_samples[2] = ps
 
 
 def Nlogpdf(yy, mean, cov):
@@ -55,9 +89,14 @@ print("\n\nComputing Folds...\n\n")
 mcmc_length = ps[0]['alpha'].shape[0]
 Ds = np.empty((mcmc_length,))
 for mcmc_iter in range(mcmc_length):
-    Ds[mcmc_iter] = -2*np.sum(\
-        Nlogpdf(complete_data[0]['test']['yy'], ps[0]['alpha'][mcmc_iter], ps[0]['Marg_cov'][mcmc_iter] ) -\
-        Nlogpdf(complete_data[0]['test']['yy'], sample_mean, sample_cov ))
+    model_1_lgpdf = Nlogpdf(complete_data[0]['test']['yy'],
+        sample_mean,
+        sample_cov)
+
+    model_2_lgpdf = Nlogpdf(complete_data[0]['test']['yy'],
+        model_posterior_samples[1][0]['alpha'][mcmc_iter],
+        model_posterior_samples[1][0]['Marg_cov'][mcmc_iter])
+    Ds[mcmc_iter] = -2*np.sum(model_1_lgpdf - model_2_lgpdf)
 
 print(Ds)
 print(Ds.shape)
