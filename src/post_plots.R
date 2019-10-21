328f165f81f676958480bfbcd70cb24e3ad91893
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-10-21 10:56:21 +0100

    added simulation2

--- src/post_plots.R
+++ src/post_plots.R
@@ -24,3 +24,24 @@ ggplot(df) +
   
 ggsave("doc/sim1.png", width = 8, height = 12, units = "cm")
 
+##############################
+##############################
+
+df <- read_csv("src/log/plot_data/post_df_sim2.csv") %>%
+  mutate(error = data - mean, 
+         index = factor(1:n()))
+
+labels = paste0('item(' ,df$J,',', df$K, ')')
+
+ggplot(df) +
+  geom_segment(aes(x = `q2.5` , xend = `q97.5`, y=index, yend=index)) +
+  geom_point(aes(y=index, x=mean), color = 'black', alpha=.5)+
+  geom_point(aes(y=index, x=data), shape=24, fill ='#0394fc', color =  '#0394fc', alpha=.5)+
+  scale_y_discrete(labels = labels)+
+  theme_fivethirtyeight()+
+  scale_colour_manual(values = c('black', '#0394fc'),
+                      labels = c("data", "data2"))
+
+ggsave("doc/sim2.png", width = 8, height = 12, units = "cm")
+
+
