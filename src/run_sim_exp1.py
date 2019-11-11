96873730404276b27e8e8e82b01211a9ee7c56c9
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-11-11 10:15:38 +0000

    added option for sim 3

--- src/run_sim_exp1.py
+++ src/run_sim_exp1.py
@@ -52,6 +52,9 @@ if args.existing_directory is None:
     elif args.sim_case == 2 :
         data = gen_data(args.nsim_data, noisy_loadings=True, off_diag_residual=False,
             random_seed = args.random_seed)
+    elif args.sim_case == 3 :
+        data = gen_data(args.nsim_data, noisy_loadings=True, off_diag_residual=True,
+            random_seed = args.random_seed)
     else:
         print("Choose simulation case {0:diag Theta, \
             1:Theta with 6 off diag elements \
