bdb5ef2156a267aacd8e1c6c79d85b0b9de3b507
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-11-15 16:44:25 +0000

    minor

--- src/run_kfold_sim_bstr.py
+++ src/run_kfold_sim_bstr.py
@@ -19,7 +19,7 @@ parser.add_argument("sim_case", help="simulation case number", type=int, default
 parser.add_argument("stan_model", help="0:full model, 1:no u's, 2: no u's no approx zero betas ", type=int, default=0)
 # Optional arguments
 parser.add_argument("-nll","--noisy_loadings_level", help="option level for cross loading magnitude", type=int, default=3)
-parser.add_argument("-bnsim","--btstr_nsim", help="random seed for data generation", type=int, default=3)
+parser.add_argument("-bnsim","--btstr_nsim", help="random seed for data generation", type=int, default=10)
 parser.add_argument("-nfl", "--n_splits", help="number of folds", type=int, default=3)
 parser.add_argument("-num_chains","--num_chains", help="number of MCMC chains", type=int, default=1)
 parser.add_argument("-nd","--nsim_data", help="data size", type=int, default=500)
