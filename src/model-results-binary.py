0cf08cb6746fcfaa86b3922987b70ffe94a3bc6b
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2019-12-05 21:36:34 -0500

    added code and notebook for PPP

--- src/model-results-binary.py
+++ src/model-results-binary.py
@@ -33,91 +33,71 @@ if bool(args.print_model):
     file.close()
 
 
-data = load_obj("stan_data", log_dir)
+data = load_obj("data", log_dir)
 ps = load_obj('ps', log_dir)
 
 num_samples = ps['alpha'].shape[0]
 
-
-post_yy = np.empty((num_samples,data['N'],data['J']))
-for i in range(num_samples):
-    if 'uu' in ps.keys():
-        post_yy[i] = ps['alpha'][i] + np.outer(ps['zz'][i], ps['beta'][i]) + ps['uu'][i]
-    else:
-        post_yy[i] = ps['alpha'][i] + np.outer(ps['zz'][i], ps['beta'][i])
-ps['yy'] = post_yy
-ps['pp'] = expit(post_yy)
-
-
-def f_score(data, score):
-    assert score>=1
+def order_data(data, zz, beta):
+    load_sign = np.sign(np.mean(beta))
     J = data.shape[1]
     df = pd.DataFrame(data)
     df.columns = ['x'+str(i) for i in range(1,J+1)]
-    df.insert(column='s', value=np.sum(data,1), loc=0)
-    res = df[(df['s'] == score)].shape[0]
-    return res
+    df.insert(column='z', value=zz*load_sign, loc=0)
+    return df.sort_values('z')
 
 
-def f_item_score(data, item, score):
-    J = data.shape[1]
-    assert score>=1
-    assert item>=1
-    assert item<=J
-    df = pd.DataFrame(data)
-    df.columns = ['x'+str(i) for i in range(1,data.shape[1]+1)]
-    df.insert(column='s', value=np.sum(data,1), loc=0)
-    res = df[(df['s'] == score) & df['x'+str(item)] == 1].shape[0]
-    return res
+def exp_k(df, k, alpha, beta, num_gprs=10):
+    load_sign = np.sign(np.mean(beta))
+    N = df.shape[0]/num_gprs
+    z_bar = np.mean(np.array_split(df,num_gprs)[k]['z'])
+    y_bar = alpha + z_bar * beta * load_sign
+    return np.round(expit(y_bar)*N,0)
 
 
-def prob_items(probs, data, score):
-    assert score>=1
-    probdf = pd.DataFrame(probs)
-    probdf.insert(column='s', value=np.sum(data,1), loc=0)
-    res = np.mean(probdf[probdf.s == score].values[:,1:], 0)
-    return res
-
-def compute_Q(probs, data):
-    J = data.shape[1]
-    fjs = np.empty((J, J))
-    pjs = np.empty((J, J))
-    fs = np.empty(J)
-    for s in range(1,J+1):
-        fs[s-1] = f_score(data, s)
-        pjs[:,s-1] = prob_items(probs, data, s)
-        for j in range(1,J+1):
-            fjs[j-1,s-1] = f_item_score(data, j, s)
-
-    # 2.3.3 Yen's Index (Q1) - p.37 of thesis
-    # res = np.empty((J,J))
-    # for s in range(J):
-    #     for j in range(J):
-    #         res[j,s] = (fjs[j,s] - fs[s]*pjs[j,s])**2/(fs[s]*pjs[j,s]*(1 - pjs[j,s]))
-
-    # 2.3.5 McKingley and Mills Index (G2) - p.38 of thesis
-    res = np.empty((J,J))
-    for s in range(J):
-        for j in range(J):
-            # res[j,s] = fjs[j,s] * (np.log(fjs[j,s])- np.log(fs[s]*pjs[j,s]))+\
-            #         (1 - fjs[j,s]) * np.log((1 - fjs[j,s])/(1 - fs[s]*pjs[j,s]))
-
-            # numerical more stable than above
-            res[j,s] = fjs[j,s] * (np.log(fjs[j,s])- np.log(fs[s]*pjs[j,s]))+\
-                   (1 - fjs[j,s]) * (np.log(fjs[j,s]-1 + 1e-10) - np.log(fs[s]*pjs[j,s]-1 + 1e-10))
-    return res
+def obs_k(df, k, num_gprs=10):
+    return np.array_split(df,num_gprs)[k].drop('z',1).sum(0).values
 
 
-def compute_all_Qs(probs, subsample_n):
-    num_samples = probs[0].shape[0]
-    J = probs[0].shape[1]
-    Ds = np.empty((num_samples,J,2))
-    for i in tqdm(range(num_samples)[::subsample_n]):
-        Ds[i,:,0] = np.sum(compute_Q(probs[i], data['DD']),0)
-        data_pred = bernoulli.rvs(probs[i])
-        Ds[i,:,1] = np.sum(compute_Q(probs[i], data_pred),0)
-        np.sum(Ds[:,:,0] < Ds[:,:,1], 0) / num_samples
-    return np.sum(Ds[:,:,0] < Ds[:,:,1], 0) / num_samples
+def obs_star_k(df, k, alpha, beta, num_gprs=10):
+    load_sign = np.sign(np.mean(beta))
+    y_star = alpha +  np.outer(df.z, beta*load_sign)
+    data_pred = pd.DataFrame(bernoulli.rvs(expit(y_star)))
+    return np.array_split(data_pred,num_gprs)[k].sum(0).values
 
-res = compute_all_Qs(ps['pp'], args.subsample)
-print(res)
+
+def G2(N, E, O):
+    if (O != 0).all():
+        return N*(O*np.log(O/E) + (1-O) * np.log((1-O)/(1-E)))
+    else:
+        return np.nan
+
+def G2_item(N, E, O, item):
+#     assert O[item] != 0
+    return N*(O[item]*np.log(O[item]/E[item]) + (1-O[item]) * np.log((1-O[item])/(1-E[item])))
+
+
+GG2 = np.empty((num_samples,10,6))
+GG2_post = np.empty((num_samples,10,6))
+N = 100
+for i in tqdm(range(1000)):
+    for k in range(10):
+        df = order_data(data['D'], ps['zz'][i], ps['beta'][i])
+        O = obs_k(df, k)
+        Ostar = obs_star_k(df, k, ps['alpha'][i], ps['beta'][i])
+        E = exp_k(df, k, ps['alpha'][i], ps['beta'][i])
+        for item in range(6):
+            if O[item] ==0:
+                GG2[i,k,item] = np.nan
+            else:
+                GG2[i,k,item] = G2_item(N, E, O, item)
+            if Ostar[item] ==0:
+                GG2_post[i,k,item] = np.nan
+            else:
+                GG2_post[i,k,item] = G2_item(N, E, Ostar, item)
+
+Des = np.empty(6)
+for item in range(6):
+    Des[item] = np.sum(pd.DataFrame(GG2[:,:,item]).sum(1) < pd.DataFrame(GG2_post[:,:,item]).sum(1))
+
+print(np.round((Des/num_samples)*100,0))
