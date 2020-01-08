5fbcf9987b2d28e07c2a74f3b9d89e253e691b5e
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2020-01-08 12:54:55 -0500

    added better logdir nameing

--- src/run_sim_bin.py
+++ src/run_sim_bin.py
@@ -30,7 +30,9 @@ args = parser.parse_args()
 ###### Create Directory or Open existing ##########
 if args.existing_directory is None:
     nowstr = datetime.datetime.now().strftime('%Y%m%d_%H%M%S_') # ISO 8601 format
-    log_dir =  "./log/"+nowstr+"%s/" % args.task_handle
+    log_dir =  "./log/"+nowstr+"%s_sim%s_m%s/"%(args.task_handle,
+        args.sim_case,
+        args.stan_model)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 else:
