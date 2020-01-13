da1bf35af458441411991784906261db26e118c1
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2020-01-13 16:42:37 -0500

    added corr in Theta

--- src/codebase/data.py
+++ src/codebase/data.py
@@ -128,7 +128,7 @@ def gen_data(nsim_data, J=6, K=2, rho =0.2, c=0.65, b=0.8,
 
 
 def gen_data_binary(nsim_data, J=6, K=2, rho =0.2, c=0.65, b=0.8,
-             off_diag_residual = False, off_diag_corr = 0.2,
+             off_diag_residual = False, off_diag_corr = 0.5,
              cross_loadings = False, cross_loadings_level = 1,
              method = 1, random_seed=None):
     if random_seed is not None:
