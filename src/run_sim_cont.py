9e8571c3b9402106b5b8a10878e140ae04ed987e
Konstantinos Vamvourellis <k.vamvourellis@lse.ac.uk>
2020-05-25 13:54:51 -0700

    minor

--- src/run_sim_cont.py
+++ src/run_sim_cont.py
@@ -81,14 +81,15 @@ if args.existing_directory is None or args.gen_data == True:
             random_seed = args.random_seed)
     elif args.sim_case == 2 :
         data = gen_data(args.nsim_data, cross_loadings=True, off_diag_residual=False,
-            cross_loadings_level = 3, random_seed = args.random_seed)
+            cross_loadings_level = 2, random_seed = args.random_seed)
     elif args.sim_case == 3 :
         data = gen_data(args.nsim_data, cross_loadings=True, off_diag_residual=True,
-            cross_loadings_level = 3, random_seed = args.random_seed)
+            cross_loadings_level = 2, random_seed = args.random_seed)
     else:
         print("Choose simulation case {0:diag Theta, \
             1:Theta with 6 off diag elements \
-            2:Noisy loadings}")
+            2:Cross loadings}\
+            3:Cross loadings  + Residual Correlated Errors}")
 
     if args.ppp_cv == 'ppp':  # run PPP
         print("\n\nN = %d, J= %d, K =%d"%(data['N'],data['J'], data['K'] ))
