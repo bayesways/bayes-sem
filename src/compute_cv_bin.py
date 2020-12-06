28b9436cf167255f3e790e3622868f27d522ffa1
Konstantinos Vamvourellis <>
2020-12-05 21:36:53 -0500

    added cv computation for any number of folds

--- src/compute_cv_bin.py
+++ src/compute_cv_bin.py
@@ -27,39 +27,40 @@ if logdir_benchmark[-1] != "/":
 
 ############################################################
 ################ Load Model Data  ##########
-complete_data = load_obj("complete_data", logdir_model)
-model_ps = dict()
+complete_data_model = load_obj("complete_data", logdir_model)
 model_ps = dict()
 model_ps[0] = load_obj('ps_0', logdir_model)
 model_ps[1] = load_obj('ps_1', logdir_model)
 model_ps[2] = load_obj('ps_2', logdir_model)
 mcmc_length = model_ps[0]['alpha'].shape[0]
 num_chains = model_ps[0]['alpha'].shape[1]
-Ds_model = np.empty((3, args.nsim_ppp, num_chains))
+Ds_model = np.empty((complete_data_model['n_splits'], args.nsim_ppp, num_chains))
 
-for fold_index in range(3):
+for fold_index in range(complete_data_model['n_splits']):
     Ds_model[fold_index] = get_lgscr(
         model_ps[fold_index],
-        complete_data[fold_index],
+        complete_data_model[fold_index],
         args.nsim_ppp
         )
 
 
 ############################################################
 ################ Load Benchmark Data  ##########
-complete_data = load_obj("complete_data", logdir_benchmark)
-benchmark_ps = dict()
+complete_data_benchmark = load_obj("complete_data", logdir_benchmark)
+
+assert complete_data_benchmark['n_splits'] == complete_data_model['n_splits']
+
 benchmark_ps = dict()
 benchmark_ps[0] = load_obj('ps_0', logdir_benchmark)
 benchmark_ps[1] = load_obj('ps_1', logdir_benchmark)
 benchmark_ps[2] = load_obj('ps_2', logdir_benchmark)
 mcmc_length = benchmark_ps[0]['alpha'].shape[0]
 num_chains = benchmark_ps[0]['alpha'].shape[1]
-Ds_benchmark = np.empty((3, args.nsim_ppp, num_chains))
-for fold_index in range(3):
+Ds_benchmark = np.empty((complete_data_benchmark['n_splits'], args.nsim_ppp, num_chains))
+for fold_index in range(complete_data_benchmark['n_splits']):
     Ds_benchmark[fold_index] = get_lgscr(
         benchmark_ps[fold_index],
-        complete_data[fold_index],
+        complete_data_benchmark[fold_index],
         args.nsim_ppp
         )
 
@@ -67,7 +68,7 @@ for fold_index in range(3):
 ################ Compare CV scores  ##########
 fold_chain_average_matrix = np.mean(Ds_model<Ds_benchmark, 1)
 print('\nChain/Fold Average %.2f'%np.mean(fold_chain_average_matrix))
-for f in range(3):
+for f in range(complete_data_model['n_splits']):
     chain_scores = fold_chain_average_matrix[f]
     print("\nFold %d Avg =  %.2f"%(
         f,
